dcca61414127d074c3d6cbefe79ac7689bcd1f62
Bayar Bogdanov <bogdanov@bizapp.ru>
2017-11-05 04:06:44 +0800

    no message

--- app/src/main/java/ru/bayar/bogdanov/myapplication/ui/contacts/ContactsFragment.java
+++ app/src/main/java/ru/bayar/bogdanov/myapplication/ui/contacts/ContactsFragment.java
@@ -1,24 +1,17 @@
 package ru.bayar.bogdanov.myapplication.ui.contacts;
 
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
 import ru.bayar.bogdanov.myapplication.R;
 
 public class ContactsFragment extends Fragment {
-
-    @BindView(R.id.hh_link_tv)
-    TextView mHhLinkTextView;
 
     @Nullable
     @Override
@@ -34,14 +27,5 @@ public class ContactsFragment extends Fragment {
     }
 
     private void initViews() {
-        mHhLinkTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String url = "https://ulan-ude.hh.ru/applicant/resumes/view?resume=248badb2ff03f0ed320039ed1f34375669497a";
-                Intent i = new Intent(Intent.ACTION_VIEW);
-                i.setData(Uri.parse(url));
-                startActivity(i);
-            }
-        });
     }
 }
